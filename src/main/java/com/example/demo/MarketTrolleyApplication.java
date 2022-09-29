bb666e31105523c0949eacd4d9d832e51905afb3
herbertt <herberttb@gmail.com>
2022-09-29 11:23:27 -0300

    Code project basis

---
+++ src/main/java/com/example/demo/MarketTrolleyApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketTrolleyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketTrolleyApplication.class, args);
+	}
+
+}
